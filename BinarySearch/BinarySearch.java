1e1a8b544a62fbad5d3d8b5f88ad6ad7a3a08d22
Anurag Anand <anuraganand577@gmail.com>
2026-01-20 01:04:01 +0530

    done Arrays,Linear Search & Binary search

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,25 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 5, 6, 7, 8};
+        int target = 5;
+        findTarget(arr, target);
+    }
+
+    public static void findTarget(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                System.out.println("target found at :" + mid);
+                return;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            }
+        }
+        System.out.println("target not found");
+    }
+}
+
